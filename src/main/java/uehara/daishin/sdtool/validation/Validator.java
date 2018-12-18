a8b7c96f8d8a85c62ec9a5ef481e1308bd3240b3
DaishinUehara <git.uehara@gmail.com>
2018-12-18 23:39:17 +0900

    Add Validation Logic

--- src/main/java/uehara/daishin/sdtool/validation/Validator.java
+++ src/main/java/uehara/daishin/sdtool/validation/Validator.java
@@ -2,11 +2,14 @@ package uehara.daishin.sdtool.validation;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Validator {
 //	public static Pattern pDateYYYYMMDD;
 	public static SimpleDateFormat pDateYYYYMMDD;
+	public static SimpleDateFormat pDateYYMMDD;
+	public static SimpleDateFormat pDateTimeYYYYMMDDHHMM;
+	public static SimpleDateFormat pDateTimeYYYYMMDDHHMMSS;
+	public static SimpleDateFormat pDateTimeYYYYMMDDHHMMSSsss;
 
 	public static boolean validation(String str,ValidationType vt) {
 		boolean ret_val=false;
@@ -16,7 +19,51 @@ public class Validator {
 				pDateYYYYMMDD = new SimpleDateFormat("yyyyMMdd");
 			}
 			try {
-				Date format = pDateYYYYMMDD.parse(str);
+				pDateYYYYMMDD.parse(str);
+				ret_val= true;
+			} catch (ParseException e) {
+				ret_val= false;
+			}
+			break;
+		case DateYYMMDD1:
+			if (null == pDateYYMMDD) {
+				pDateYYMMDD = new SimpleDateFormat("yyMMdd");
+			}
+			try {
+				pDateYYMMDD.parse(str);
+				ret_val= true;
+			} catch (ParseException e) {
+				ret_val= false;
+			}
+			break;
+		case DateTimeYYYYMMDDHHMM1:
+			if (null == pDateTimeYYYYMMDDHHMM) {
+				pDateTimeYYYYMMDDHHMM = new SimpleDateFormat("yyyyMMddHHmm");
+			}
+			try {
+				pDateTimeYYYYMMDDHHMM.parse(str);
+				ret_val= true;
+			} catch (ParseException e) {
+				ret_val= false;
+			}
+			break;
+		case DateTimeYYYYMMDDHHMMSS1:
+			if (null == pDateTimeYYYYMMDDHHMMSS) {
+				pDateTimeYYYYMMDDHHMMSS = new SimpleDateFormat("yyyyMMddHHmmss");
+			}
+			try {
+				pDateTimeYYYYMMDDHHMMSS.parse(str);
+				ret_val= true;
+			} catch (ParseException e) {
+				ret_val= false;
+			}
+			break;
+		case DateTimeYYYYMMDDHHMMSSsss1:
+			if (null == pDateTimeYYYYMMDDHHMMSSsss) {
+				pDateTimeYYYYMMDDHHMMSSsss = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+			}
+			try {
+				pDateTimeYYYYMMDDHHMMSSsss.parse(str);
 				ret_val= true;
 			} catch (ParseException e) {
 				ret_val= false;
